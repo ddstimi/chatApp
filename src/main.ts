26217bc022a6a4174584cbde2f155b6c3933f20c
Dudas Timea <h257330@stud.u-szeged.hu>
2024-12-05 13:59:10 +0100

    firebase plus auth service

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes'; // Assuming this is where your routes are defined
+import { AngularFireModule } from '@angular/fire/compat';
+import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { environment } from './environments/environment';
+
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(AngularFireModule.initializeApp(environment.firebase))
   ]
 }).catch(err => console.error(err));
